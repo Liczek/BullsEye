fa0b53763e2fe61d364ed69a7fd77945a7ffcf1d
Paweł Liczmański <paweliczek@gmail.com>
2016-10-29 23:36:02 +0200

    podstawa

---
+++ BullsEye/BullsEye/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BullsEye
+//
+//  Created by Paweł Liczmański on 29.10.2016.
+//  Copyright © 2016 Paweł Liczmański. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
